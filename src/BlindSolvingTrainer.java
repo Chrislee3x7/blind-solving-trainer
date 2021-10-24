0b50989aa92acc807b1c76399e89ac28e1f478e5
Chrislee3x7 <32285657+Chrislee3x7@users.noreply.github.com>
2021-10-24 15:24:54 -0400

    initial commit, starting BlindSolvingTrainer.java

---
+++ src/BlindSolvingTrainer.java
@@ -0,0 +1,15 @@
+/**
+ * BlindSolvingTrainer
+ *
+ * A program that allows user to train the memorization of corresponding letters
+ * of the pieces on the 3x3 Rubik's Cube
+ *
+ * @author Chrislee3x7
+ * @version 10/24/21
+ */
+public class BlindSolvingTrainer {
+
+    public static void main(String[] args) {
+        
+    }
+}
